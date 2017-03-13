8e3d6639c9e45735b2eae294dbd65f94119bcc6e
rosemillard <rosemillard@Roses-MacBook-Pro.local>
2017-03-12 19:43:40 -1000

    ajax

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+     url: 'https://www.codeschool.com/users/RoseMillard',
+     dataType: 'jsonp',
+     success: function(response) {
+       // handle response
+     }
+   });
 
 });
