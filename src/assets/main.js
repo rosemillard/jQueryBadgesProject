9101ea29b5a2ac29fd5d9d417a01a9c538700b69
rosemillard <rosemillard@Roses-MacBook-Pro.local>
2017-03-12 20:45:32 -1000

    all

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,26 +1,37 @@
 $(function() {
 
+  // your code will go here
   $.ajax({
-     url: 'https://www.codeschool.com/users/RoseMillard.json',
-     dataType: 'jsonp',
-     success: function(response) {
-      addCourses(response.courses.completed);
-     }
-   });
-
-
-  function addCourses(courses) {
-
-    var $badges = $('#badges');
-
-    courses.forEach(function(course) {
-
-        $('<div />', {
-          'class': 'course'
-        }).appendTo($badges);
-    })
-
-
+  url: 'https://www.codeschool.com/users/jim-arms.json',
+  dataType: 'jsonp',
+  success: function(response) {
+    addCourses(response.courses.completed);
   }
+});
 
-  });
+function addCourses(courses){
+
+   var $badges = $('#badges');
+
+   courses.forEach(function(course){
+      var $course = $('<div />', {
+         'class':'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+         text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+         src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+         'class': 'btn btn-primary',
+         target: '_blank',
+         href: course.url,
+         text: 'See it on Code School'
+      }).appendTo($course);
+   })
+}
+});
